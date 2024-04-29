5269386f61fdb4a24c7c2afbbc6584f999946d18
bas1c <68533770+bas1c1@users.noreply.github.com>
2024-04-29 21:33:09 +0300

    v1.1 (iso yeeeee)

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#this stuff i need so yeah
+
 #mkdir -p amuros/build/sources
 #mkdir -p amuros/build/downloads
 #mkdir -p amuros/build/out
@@ -25,7 +27,7 @@ fi
 
 if [ ! -d "amuros/linux" ]; then
   mkdir -p amuros/linux
-  sudo apt install --yes make build-essential bc bison flex libssl-dev libelf-dev wget cpio fdisk extlinux dosfstools qemu-system-x86
+  sudo apt install --yes make build-essential bc bison flex libssl-dev libelf-dev wget cpio fdisk extlinux dosfstools qemu-system-x86 xorriso
 fi
 
 if [ ! -d "amuros/build/downloads" ]; then
@@ -67,23 +69,31 @@ cd ..
 find . | cpio -o -H newc > ../../../amuros/linux/initrd-busybox-1.35.0.img
 cd ../../../amuros/linux
 
-sudo dd if=/dev/zero of=boot-disk.img bs=1024K count=50
-sudo echo "type=83,bootable" | sudo sfdisk boot-disk.img
-sudo losetup -D
-LOOP_DEVICE=$(losetup -f)
-sudo losetup -o $(expr 512 \* 2048) ${LOOP_DEVICE} boot-disk.img
-sudo mkfs.vfat ${LOOP_DEVICE}
-sudo mkdir -p /mnt/os
-sudo mount -t auto ${LOOP_DEVICE} /mnt/os
-sudo cp vmlinuz-5.15.79 initrd-busybox-1.35.0.img /mnt/os
-sudo mkdir -p /mnt/os/boot
-sudo extlinux --install /mnt/os/boot
-sudo cp ../../syslinux.cfg /mnt/os/boot/syslinux.cfg
-sudo umount /mnt/os
-sudo losetup -D
-sudo dd if=/usr/lib/syslinux/mbr/mbr.bin of=boot-disk.img bs=440 count=1 conv=notrunc
+sudo cp -r ../../iso iso
+sudo cp initrd-busybox-1.35.0.img iso/boot
+sudo cp vmlinuz-5.15.79 iso/boot
+sudo grub-mkrescue -o amuros.iso iso
+sudo rm -rf iso
 
-#uncomment this if u need
+#generating .img, u dont need this shit
+
+#sudo dd if=/dev/zero of=boot-disk.img bs=1024K count=50
+#sudo echo "type=83,bootable" | sudo sfdisk boot-disk.img
+#sudo losetup -D
+#LOOP_DEVICE=$(losetup -f)
+#sudo losetup -o $(expr 512 \* 2048) ${LOOP_DEVICE} boot-disk.img
+#sudo mkfs.vfat ${LOOP_DEVICE}
+#sudo mkdir -p /mnt/os
+#sudo mount -t auto ${LOOP_DEVICE} /mnt/os
+#sudo cp vmlinuz-5.15.79 initrd-busybox-1.35.0.img /mnt/os
+#sudo mkdir -p /mnt/os/boot
+#sudo extlinux --install /mnt/os/boot
+#sudo cp ../../syslinux.cfg /mnt/os/boot/syslinux.cfg
+#sudo umount /mnt/os
+#sudo losetup -D
+#sudo dd if=/usr/lib/syslinux/mbr/mbr.bin of=boot-disk.img bs=440 count=1 conv=notrunc
+
+#uncomment this if u need (DONT DO THIS)
 #qemu-system-x86_64 -kernel vmlinuz-5.15.79 -initrd initrd-busybox-1.35.0.img -nographic -append 'console=ttyS0'
 
-sudo qemu-system-x86_64 boot-disk.img
+#sudo qemu-system-x86_64 boot-disk.img
